02740cb5cbf2b5b2dba402bb45c16b832e4ae611
chiangyiyang <chiangyiyang@gmail.com>
2017-05-16 03:37:36 +0200

    init commit

---
+++ move2areas.py
@@ -0,0 +1,41 @@
+from qgis.utils import iface
+import time
+
+rx = range(121,123)
+ry = range(22,24)
+cx = 121
+cy = 22
+dd = 1
+isDone = False
+
+mycvs = iface.mapCanvas()
+
+def myfun():
+    print("do somthing")
+    global cx, cy, rx, ry, dd, isDone, myfun
+    if (cx+dd) not in rx:
+        if(cy+dd) not in ry:
+            isDone = True
+        else:
+            cy = cy+dd
+            cx = rx[0]
+    else:
+        cx = cx+dd
+    if not isDone:
+        zoom2area(mycvs, cx, cy, dd)
+    else:
+        mycvs.mapCanvasRefreshed.disconnect(myfun)
+
+
+mycvs.mapCanvasRefreshed.connect(myfun)
+
+
+def zoom2area(canvas, x, y, d):
+    canvas.setExtent(
+            QgsRectangle(
+                x, y, x+d, y+d
+                ))
+    canvas.refresh()
+    print("zoom to : ", x, y, x+d, y+d)
+
+zoom2area(mycvs, cx, cy, dd)
